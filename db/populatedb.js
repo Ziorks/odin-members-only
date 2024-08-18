72e4d201232b56eb0fa39af7a7aa6711a7ac35d9
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-08-17 21:38:45 -0500

    Add populatedb sql commands

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,15 +2,33 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const SQL = `
-CREATE TABLE IF NOT EXISTS exampleTable (
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+  "sess" json NOT NULL,
+  "expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+CREATE INDEX "IDX_session_expire" ON "session" ("expire");
+
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    exampleColumn VARCHAR ( 255 )
+    first_name VARCHAR ( 255 ) NOT NULL,
+    last_name VARCHAR ( 255 ) NOT NULL,
+    username VARCHAR ( 255 ) NOT NULL,
+    password VARCHAR ( 255 ) NOT NULL,
+    is_admin BOOLEAN NOT NULL
 );
 
-INSERT INTO exampleTable (exampleColumn)
-VALUES
-    ('value1'),
-    ('value2');
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR ( 255 ) NOT NULL,
+    message VARCHAR ( 255 ) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER REFERENCES users(id)
+);
 `;
 
 async function main() {
@@ -18,9 +36,9 @@ async function main() {
   const client = new Client({
     connectionString: process.env.DATABASE_URL, //postgresql://<roleName>:<rolePassword>@localhost:<databasePort>/<databaseName>
   });
-  client.connect();
-  client.query(SQL);
-  client.end();
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
   console.log("done");
 }
 
